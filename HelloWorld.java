76467d0ca7e3d23e7b5fcce482c2b09d4685361a
LarryZRL <40879000+LarryZRL@users.noreply.github.com>
2020-03-24 23:07:03 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("hello");
-        System.out.println("ghhiiii");
+        System.out.println("helloworld");
+        System.out.println("ghhiiiifff");
         
     }
 }
